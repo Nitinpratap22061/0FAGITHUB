5a665f0d3e3aec50e30a1f7f3fa172def74d50ff
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-03-16 14:52:49 +0530

    Update passportConfig.js

--- backends/config/passportConfig.js
+++ backends/config/passportConfig.js
@@ -8,7 +8,7 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "https://zerofagithub.onrender.com/auth/github/callback",
+      callbackURL:"https://zerofagithub.onrender.com/auth/github/callback",
       scope: ["user:email", "repo"], // Request access to user email and repositories
     },
     async (accessToken, refreshToken, profile, done) => {
