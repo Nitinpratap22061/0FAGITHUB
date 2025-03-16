6a1f6d0dd69f7994276713cfc5b534a1870b6f42
Nitin Pratap <122565643+Nitinpratap22061@users.noreply.github.com>
2025-03-16 15:07:30 +0530

    Update authRoutes.js

--- backends/routes/authRoutes.js
+++ backends/routes/authRoutes.js
@@ -4,16 +4,13 @@ const router = express.Router();
 
 router.get("/github", passport.authenticate("github", { scope: ["user:email", "repo"] }));
 
+router.get("/github", passport.authenticate("github", { scope: ["user:email", "repo"] }));
+
 router.get(
   "/github/callback",
   passport.authenticate("github", { failureRedirect: "/" }),
   (req, res) => {
-    console.log("GitHub callback triggered");
     const { user, token, accessToken } = req.user;
-    console.log("User:", user);
-    console.log("Token:", token);
-    console.log("Access Token:", accessToken);
-
     if (user.isNewUser) {
       res.redirect(`${process.env.FRONTEND_URL}/intro?token=${token}&accessToken=${accessToken}`);
     } else {
@@ -21,5 +18,4 @@ router.get(
     }
   }
 );
-
 module.exports = router;
